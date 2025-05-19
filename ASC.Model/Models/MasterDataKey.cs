d193f214d79087ebad4f166f8041ff10c3cdddf8
Nguyễn Dương <vietduong1122@outlook.com>
2025-05-19 22:20:24 +0700

    Lab7

---
+++ ASC.Model/Models/MasterDataKey.cs
@@ -0,0 +1,21 @@
+﻿using ASC.Model.BaseTypes;
+
+namespace ASC.Model.Models
+{
+    public class MasterDataKey : BaseEntity
+    {
+        public MasterDataKey() { }
+
+        public MasterDataKey(string key, string updatedBy)
+        {
+            this.RowKey = Guid.NewGuid().ToString();
+            this.PartitionKey = key;
+            //this.UpdatedBy = updatedBy; // Cung cấp giá trị cho UpdatedBy
+        }
+
+        public bool IsActive { get; set; }
+
+        public string Name { get; set; }
+    }
+
+}
